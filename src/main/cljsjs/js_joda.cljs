025aa1ca75d167098b8ba1208a2604cafe3d2a67
Henry Widd <henryw374@gmail.com>
2018-11-12 21:07:02 +0000

    shims for cljs js-joda packages (#10)

---
+++ src/main/cljsjs/js_joda.cljs
@@ -0,0 +1,5 @@
+(ns cljsjs.js-joda
+  (:require ["js-joda" :as js-joda]))
+
+(js/goog.exportSymbol "JSJoda" js-joda)
+
---
+++ src/main/cljsjs/js_joda_locale_en_us.cljs
@@ -0,0 +1,5 @@
+(ns cljsjs.js-joda-locale-en-us
+  (:require ["@js-joda/locale_en-us" :as js-joda-locale]))
+
+(js/goog.exportSymbol "JSJodaLocale" js-joda-locale)
+
